08dd4a0a7137fcdb0c3dbd246c91e616749243d4
Nicolas F. Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-11-02 05:58:16 +0000

    OTI/Core 445120, OTI/XMI 445123
    git-svn-id: https://www.omgwiki.org/repos/TIWG/Eclipse%20Scala%20Projects/org.omg.oti.uml.loader@445124 b05f3425-8805-43f2-8a1d-5852a8693556

--- project/Versions.scala
+++ project/Versions.scala
@@ -20,12 +20,12 @@ object Versions {
   // OTI Core version
     
   val oti_core_prefix = version_prefix
-  val oti_core_suffix = "445107"
+  val oti_core_suffix = "445120"
   val oti_core_version = oti_core_prefix+"-"+oti_core_suffix
 
   // OTI Canonical XMI version
     
   val oti_canonicalXMI_prefix = version_prefix
-  val oti_canonicalXMI_suffix = "445110"
+  val oti_canonicalXMI_suffix = "445123"
   val oti_canonicalXMI_version = oti_canonicalXMI_prefix+"-"+oti_canonicalXMI_suffix
 }
