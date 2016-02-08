c971def416d20fc66bb209c50e2c02bbdc22f44b
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-02-08 05:47:00 +0000

    IMCE sbt plugin

--- project/Versions_oti_uml_canonical_xmi_serialization.scala
+++ project/Versions_oti_uml_canonical_xmi_serialization.scala
@@ -1,5 +1,5 @@
 object Versions_oti_uml_canonical_xmi_serialization {
 
   // https://github.jpl.nasa.gov/imce/org.omg.oti.uml.canonical_xmi.serialization
-  val version="445382.5"
+  val version="445382.6"
 }
